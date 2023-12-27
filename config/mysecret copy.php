3accccb9d851f693479faaa3f1ab85a202e6492a
eua-angkoon-n <Eua-angkoon.n@jwdcoldchain.com>
2023-12-27 10:16:28 +0700

    mysecret

---
+++ config/mysecret copy.php
@@ -0,0 +1,18 @@
+<?php
+class MySecret
+{
+    public static $conNow = 'db'; // local , db
+    public static $dbDatabase = 'xxxxx'; 
+    public static $dbDatabaseLogin = 'xxxxx'; 
+    public static $dbUser = 'xxxxx';
+    public static $dbPass = 'xxxxx';
+    public static $dbHost = 'xxxxx'; 
+    public static $dbPort = 'xxxxx';
+
+    public static $LocalDatabase = 'xxxxx';
+    public static $LocalDatabaseLogin = 'xxxxx';
+    public static $LocalUser = 'root';
+    public static $LocalPass = '';
+    public static $LocalHost = 'localhost';
+    public static $LocalPort = '';
+}
