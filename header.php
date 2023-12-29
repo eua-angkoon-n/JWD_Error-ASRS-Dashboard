c8b91d392392f99fc2a98514fe9d395d8704240f
eua-angkoon-n <Eua-angkoon.n@jwdcoldchain.com>
2023-12-29 09:22:57 +0700

    add web icon

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?PHP echo $title_site; ?></title>
 
+<!-- Favicon -->
+<link rel="icon" type="image/webp" href="dist/img/favicon.webp">
+
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
